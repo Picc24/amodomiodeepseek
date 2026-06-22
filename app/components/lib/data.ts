8ceb31836c5cdefaed4a29786236727a1ec771de
Picc24 <supermanpicci@gmail.com>
2026-06-22 15:57:17 +0200

    Create data.ts

---
+++ app/components/lib/data.ts
@@ -0,0 +1,9 @@
+export const CONFIG = {
+  name: "A Modo Mio da Adri",
+  phone: "3277119566",
+  phoneFormatted: "327 711 9566",
+  address: "Via Giuseppe Garibaldi, 56",
+  city: "Stradella (PV)",
+  whatsappLink: "https://wa.me/393277119566",
+  instagram: "https://www.instagram.com/ristorante_amodomio/",
+};
